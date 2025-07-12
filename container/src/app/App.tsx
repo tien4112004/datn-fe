1e317529a48e4cd778014caffab0fbba68d59500
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-07-12 20:17:14 +0700

    feat: add data mode routing

--- container/src/app/App.tsx
+++ container/src/app/App.tsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PresentationPage from '@/features/presentation/page';
-import { CardsDemo } from '@/components/cards';
+import { Outlet } from 'react-router-dom';
 import Nav from '@/shared/layouts/Nav';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<CardsDemo />} />
-        <Route path="/presentation" element={<PresentationPage />} />
-      </Routes>
-    </BrowserRouter>
+      <Outlet />
+    </>
   );
 }
--- container/src/app/main.tsx
+++ container/src/app/main.tsx
@@ -1,17 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { StagewiseToolbar } from '@stagewise/toolbar-react';
 import ReactPlugin from '@stagewise-plugins/react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import App from './App.tsx';
+import { CardsDemo } from '@/shared/components/cards';
+import PresentationPage from '@/features/presentation/page';
 
 const toolbarConfig = {
   plugins: [ReactPlugin],
 };
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <CardsDemo />,
+      },
+      {
+        path: 'presentation',
+        element: <PresentationPage />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <StagewiseToolbar config={toolbarConfig} />
   </StrictMode>
 );

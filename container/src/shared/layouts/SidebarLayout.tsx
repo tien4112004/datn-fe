948e2aec89251a167ccf9c527a530782c9109fc6
Vuong Tien Nguyen Bui <vuongtiennguyenbui@gmail.com>
2025-07-20 14:01:26 +0700

    fix: add missing code from rebase

--- container/src/shared/layouts/SidebarLayout.tsx
+++ container/src/shared/layouts/SidebarLayout.tsx
@@ -4,11 +4,13 @@ import { SidebarInset, SidebarProvider, useSidebar } from '../components/ui/side
 import { AppSidebar } from '../components/navigation/AppSidebar';
 import { subscribe, unsubscribe } from '@/shared/lib/event';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 function NavLayoutContent() {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
   const { state, toggleSidebar } = useSidebar();
+  const { t } = useTranslation();
 
   const hideSidebar = () => {
     if (state === "expanded") { toggleSidebar(); }
@@ -28,7 +30,7 @@ function NavLayoutContent() {
     <>
       <AppSidebar />
       <SidebarInset>
-        {isLoading && <GlobalSpinner text="Loading page..." />}
+        {isLoading && <GlobalSpinner text={t('page')} />}
         <Outlet />
       </SidebarInset>
     </>

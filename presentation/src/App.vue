4d38d91bb0e11e74f5a5e9f2002e14a1fd45935e
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-07-22 17:00:27 +0700

    [DATN-49] Setup CD Pipeline (#10)
    * feat: disable vercel git automation and replace with cli
    
    * test: cd
    
    chore: rename cd pipeline
    
    fix: pipeline
    
    fix: get url
    
    fix: comment permission
    
    fix: read permission
    
    * feat: combine all into 1 jobs
    
    * fix: pipeline
    
    fix: pipeline
    
    fix: pipeline
    
    feat: delete previous comments
    
    fix: pipeline
    
    fix: run on PR push
    
    fix: deployments
    
    * feat: enable caching best practices
    
    chore: small change
    
    fix: pipeline
    
    fix: cache
    
    test: pipeline
    
    test: pipeline
    
    test: pipeline
    
    test: pipeline
    
    test: pipeline
    
    fix: this should be the final commit or I would die trying
    
    fix: i promise this is the last one
    
    fix: previous stupid code
    
    * feat: add cd production

--- presentation/src/App.vue
+++ presentation/src/App.vue
@@ -18,7 +18,7 @@ import { LOCALSTORAGE_KEY_DISCARDED_DB } from '@/configs/storage';
 import { deleteDiscardedDB } from '@/utils/database';
 import { isPC } from '@/utils/common';
 import api from '@/services';
-
+//
 import Editor from './views/Editor/index.vue';
 import Screen from './views/Screen/index.vue';
 import Mobile from './views/Mobile/index.vue';

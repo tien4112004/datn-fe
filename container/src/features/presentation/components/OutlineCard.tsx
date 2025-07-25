4dbfc6425c6a23e3ec40d4789f0bb31aa20450ec
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-07-25 20:39:27 +0700

    feat: extract props for OutlineCard

--- container/src/features/presentation/components/OutlineCard.tsx
+++ container/src/features/presentation/components/OutlineCard.tsx
@@ -1,29 +1,36 @@
 import RichTextEditor from '@/shared/components/rte/RichTextEditor';
 import { useRichTextEditor } from '@/shared/components/rte/useRichTextEditor';
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/components/ui/card';
+import type { PartialBlock } from '@blocknote/core';
 
-const OutlineCard = () => {
+const OutlineCard = ({
+  title = 'Outline',
+  className = '',
+  texts = [
+    {
+      type: 'heading',
+      content: 'Welcome to this demo!',
+    },
+    {
+      type: 'paragraph',
+      content:
+        'This is a sample text for the outline card. You can edit this content using the rich text editor.',
+    },
+  ],
+}: {
+  title?: string;
+  className?: string;
+  texts?: PartialBlock[];
+}) => {
   const editor = useRichTextEditor({
-    initialContent: [
-      {
-        type: 'paragraph',
-        content: 'Welcome to this demo!',
-      },
-      {
-        type: 'paragraph',
-        content:
-          'There are no menus or toolbars in this editor, but you can still markup text using keyboard shortcuts.',
-      },
-      {
-        type: 'paragraph',
-        content: 'Try making text bold with Ctrl+B/Cmd+B or undo with Ctrl+Z/Cmd+Z.',
-      },
-    ],
+    initialContent: texts,
   });
   return (
-    <Card className="border-primary flex min-h-24 w-full flex-row gap-4 bg-white p-0 shadow-md transition-shadow duration-300 hover:shadow-lg">
+    <Card
+      className={`border-primary flex min-h-24 w-full flex-row gap-4 bg-white p-0 shadow-md transition-shadow duration-300 hover:shadow-lg ${className}`}
+    >
       <CardHeader className="bg-accent w-24 rounded-l-xl p-4 text-center">
-        <CardTitle>abc</CardTitle>
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardContent className="flex-start flex-1 p-4">
         <RichTextEditor editor={editor} sideMenu={false} className="-mx-2" />

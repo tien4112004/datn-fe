6224a98568fd1579def35beba44b7ad56f03932d
Vuong Tien Nguyen Bui <vuongtiennguyenbui@gmail.com>
2025-10-02 19:27:34 +0700

    fix: toggle options save history

--- container/src/features/image/pages/CreateImagePage.tsx
+++ container/src/features/image/pages/CreateImagePage.tsx
@@ -43,7 +43,7 @@ const CreateImagePage = () => {
     } else {
       newParams.delete('advanced');
     }
-    setSearchParams(newParams);
+    setSearchParams(newParams, { replace: true });
   };
 
   // const topicValue = watch('topic') as string;

d8d7bda8ebb06b2214d32ede3db57aefdf9c032f
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-10-11 18:20:43 +0700

    fix: build error

--- container/src/features/presentation/utils/index.ts
+++ container/src/features/presentation/utils/index.ts
@@ -1,12 +1,3 @@
 export { default as splitMarkdownToOutlineItems } from './splitMarkdownToOutlineItems';
 export { default as mapOutlineItemsToMarkdown } from './mapOutlineItemsToMarkdown';
-export {
-  getDefaultPresentationTheme,
-  getBusinessTheme,
-  getEducationTheme,
-  getCreativeTheme,
-  getMinimalTheme,
-  getModernTheme,
-  getClassicTheme,
-  getPresentationThemes,
-} from './themes';
+export { getDefaultPresentationTheme, getPresentationThemes } from './themes';

1494e9be2ff079a661a2374a39f95c75ed76403a
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-10-26 18:55:38 +0700

    feat: localization

--- presentation/src/locales/en/header.ts
+++ presentation/src/locales/en/header.ts
@@ -43,5 +43,24 @@ export default {
     meta: {
       copyright: 'Copyright © 2020-PRESENT pipipi-pikachu',
     },
+
+    // Share Menu
+    shareMenu: {
+      title: 'Share "Presentation"',
+      addPeople: 'Add people, groups, or calendar events',
+      peopleWithAccess: 'People with access',
+      generalAccess: 'General access',
+      copyLink: 'Copy link',
+      linkCopied: 'Link copied to clipboard!',
+      restricted: 'Restricted',
+      restrictedDescription: 'Only people with access can open',
+      anyoneWithLink: 'Anyone with the link',
+      anyoneWithLinkDescription: 'Anyone on the internet can view',
+      viewer: 'Viewer',
+      viewerDescription: 'Can view only',
+      commenter: 'Commenter',
+      commenterDescription: 'Can view and comment',
+      defaultPermissionForAnyone: 'Default permission for "Anyone" access',
+    },
   },
 };

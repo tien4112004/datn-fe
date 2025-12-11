df5f937bd33f76341044a9077e45cdc92cc44552
Luu Thai Toan <22120375@student.hcmus.edu.vn>
2025-12-11 16:11:56 +0700

    fix: final fix

--- presentation/src/components/Spinner.vue
+++ presentation/src/components/Spinner.vue
@@ -1,7 +1,12 @@
 <template>
-  <div v-if="isGenerating" class="tw-fixed tw-inset-0 tw-z-[9999] tw-flex tw-items-center tw-justify-center tw-bg-gray-100/80 tw-backdrop-blur-[4px]">
+  <div
+    v-if="isGenerating"
+    class="tw-fixed tw-inset-0 tw-z-[9999] tw-flex tw-items-center tw-justify-center tw-bg-gray-100/80 tw-backdrop-blur-[4px]"
+  >
     <div class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-w-[200px] tw-h-[200px]">
-      <div class="tw-w-9 tw-h-9 tw-border-4 tw-border-blue-600 tw-border-t-transparent tw-rounded-full tw-animate-spin"></div>
+      <div
+        class="tw-w-9 tw-h-9 tw-border-4 tw-border-blue-600 tw-border-t-transparent tw-rounded-full tw-animate-spin"
+      ></div>
       <div class="tw-mt-5 tw-text-blue-600 tw-font-medium tw-text-center">{{ message }}</div>
     </div>
   </div>

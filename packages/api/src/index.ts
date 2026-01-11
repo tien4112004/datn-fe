62b2e46b25582e21bbdf987bfccdae771f347ce0
Tondeptrai23 <luutoan231104@gmail.com>
2026-01-11 19:28:01 +0700

    feat: mindmap detail works

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,6 +2,8 @@ export { default as api } from './client';
 export { default as webViewApi } from './webViewClient';
 export { webViewTokenManager } from './webViewTokenManager';
 export type { StreamableAxiosInstance } from './client';
+export { default as webviewApi } from './webview-client';
 export * from './types';
 export * from './api-mode';
 export * from './backend-url';
+export * from './webview-mode';
